8666cf6725993511f7a2afb81503e25e24a4f9c2
thuythuytiennguyen <thuytienthuytien134@gmail.com>
2022-05-01 15:04:36 -0700

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 const init = () => {
-  
+    const inputForm = document.querySelector('form');
+    inputForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+        console.log(event.target.children[1].value);
+        fetch(`http://localhost:3000/movies/${event.target.children[1].value}`)
+        .then(response => response.json())
+        .then(data => {
+            const title = document.querySelector('section#movieDetails h4');
+            const summary = document.querySelector('section#movieDetails p');
+            title.innerText = data.title
+            summary.innerText = data.summary
+        })
+    });
 }
 
 document.addEventListener('DOMContentLoaded', init);
